2f292bc5054e0447db06bc607c12b3c89f264ac4
ChrisMontez <64094989+ChrisMontez@users.noreply.github.com>
2021-01-22 17:43:31 -0600

    --

--- Fundamentals/Vectors/Definition.R
+++ Fundamentals/Vectors/Definition.R
@@ -3,13 +3,15 @@ NumericIntegerVector <- c(1L, 2L, 3L, 4L)
 CharVector <- c("a", "b", "c")
 CharVector <- c("a", "b", 1)
 
-#Sequence
 # seq(start,stop,step)
 seq(1,100)
 1:100
 
-#Replicate 
 #rep("", iteration)
 
 rep(1,5)
+
+#Use Vector[:] format to access specific values
+
+
 
