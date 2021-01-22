eefe6052f4fb6f413e222367046ca0e73de801f5
ChrisMontez <64094989+ChrisMontez@users.noreply.github.com>
2021-01-22 17:18:42 -0600

    --

---
+++ Fundamentals/vectors.R
@@ -0,0 +1,15 @@
+DoubleVector <- c(1,2,3,4,5)
+NumericIntegerVector <- c(1L, 2L, 3L, 4L)
+CharVector <- c("a", "b", "c")
+CharVector <- c("a", "b", 1)
+
+#Sequence
+# seq(start,stop,step)
+seq(1,100)
+1:100
+
+#Replicate 
+#rep("", iteration)
+
+rep(1,5)
+
